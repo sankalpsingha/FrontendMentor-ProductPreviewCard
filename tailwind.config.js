ed60f553d36a8239f7367cc15e1322c3eeb8f249
sankalpsingha <sankalp.singha@autobahn-security.com>
2023-04-04 03:45:20 +0700

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
+    screens: {
+      'sm': '375px',
+      'xl': '1440px',
+    },
+    colors: {
+      'primary-dark-cyan': 'hsl(158, 36%, 37%)',
+      'primary-cream': 'hsl(30, 38%, 92%)',
+      'neutral-very-dark-blue': 'hsl(212, 21%, 14%)',
+      'neutral-dark-grayish-blue': 'hsl(228, 12%, 48%)',
+      'white': 'hsl(0, 0%, 100%)',
+    },
+    fontFamily: {
+      'body': ['Montserrat', 'sans-serif'],
+      'heading': ['Fraunces', 'serif'],
+    },
+    fontWeight: {
+      'regular': 500,
+      'bold': 700,
+    },
     extend: {},
   },
   plugins: [],
